ecbabb8e964df61afda0a019f52ca18e2269c26b
TudorCe <101194278+TudorCe@users.noreply.github.com>
2024-03-11 15:37:33 +0200

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -12,10 +12,21 @@ const Home = (props) => {
         <meta property="og:title" content="Known Homely Stingray" />
       </Helmet>
       <h1>Heading</h1>
-      <span>Text</span>
-      <h1>Heading</h1>
-      <span>Text</span>
-      <h1>Heading</h1>
+      <video
+        src="/vyber-fotek-do-par-minut_klicove-snimky2.mp4"
+        poster="https://play.teleporthq.io/static/svg/videoposter.svg"
+        className="home-video"
+      ></video>
+      <video
+        src="/vyber-fotek-do-par-minut_klicove-snimky1.mp4"
+        poster="https://play.teleporthq.io/static/svg/videoposter.svg"
+        className="home-video1"
+      ></video>
+      <video
+        src="/vyber-fotek-do-par-minut_klicove-snimky.mp4"
+        poster="https://play.teleporthq.io/static/svg/videoposter.svg"
+        className="home-video2"
+      ></video>
     </div>
   )
 }
